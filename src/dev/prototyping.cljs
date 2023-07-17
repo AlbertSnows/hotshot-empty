84f9cd8d06aec81d86f49931aba68838381038d3
Albert Snow <ajsnow2012@gmail.com>
2023-07-17 13:41:48 -0500

    more setup

--- src/dev/prototyping.cljs
+++ src/dev/prototyping.cljs
@@ -1,11 +1,22 @@
-(ns prototyping)
+(ns dev.prototyping)
 
-{
-        'made_shots': ['green1', 'gray2', 'red2'],
-        'attempted_shots': 
- ['green1', 'gray2', 'blue2', 'red2']
-    }
+;; Calva dev instructions
+; == You Control what is Evaluated ==
+;; Please note that Calva never evaluates your code
+;; unless you explicitly ask for it. So, except for
+;; this file, you will have to load files you open
+;; yourself. Make it a habit to do this, because
+;; sometimes things don't work, and they fail in 
+;; peculiar ways, when your file is not loaded.
+
+;; Try it with this file: `Ctrl+Alt+C Enter`.
+;; The result of loading a file is whatever is the
+;; last top level form in the file.
+
+(defn test-function [params]
+  "I don't do anything...")
 
 (comment
+  (vec '("green1", "gray2", "red2"))
   (+ 1 1)
   )
