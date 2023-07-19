78a1dd916e2893c9830bafe3dde7bccee190ea95
Albert Snow <ajsnow2012@gmail.com>
2023-07-18 20:44:22 -0500

    showing output in terminal

--- src/main/router.cljs
+++ src/main/router.cljs
@@ -2,11 +2,17 @@
             [hotshot :refer [calc-hotshot-results]]
             [utility :refer [convert-json-to-clj-data]]))
 
+(enable-console-print!)
+
 (defn get-hotshot-score [hotshot-data]
   (let [parsed-json (convert-json-to-clj-data hotshot-data)
-        output 
-        (:final-scores (calc-hotshot-results (:body parsed-json)))
+        rounds (:body parsed-json)
+        number-of-rounds (count rounds)
+        output (if (> number-of-rounds 0) 
+                 (:final-scores (calc-hotshot-results (:body parsed-json)))
+                 [0])
         ]
+    (prn output)
     output))
 
 (comment 
@@ -14,5 +20,6 @@
    "{'body':
        [ {
             'made_shots': ['green1', 'yellow1', 'gray1', 'blue2', 'red2'],
-            'attempted_shots': ['green1', 'yellow1', 'gray1', 'blue2', 'red2']}]}")
+            'attempted_shots': ['green1', 'yellow1', 'gray1', 'blue2', 'red2']}]}"
+   )
   )
